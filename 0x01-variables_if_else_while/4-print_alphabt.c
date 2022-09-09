2efcdc5d49fb04d6b0316fbe892ac62f26d00204
Malachy Nwafor <nwaformalachy2@gmail.com>
2022-09-09 12:46:35 -0700

    A program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch != 'e' && ch != 'q')
+		if (ch != 'e' && ch != 'q'i)
 		{
 			putchar(ch);
 		}
@@ -21,3 +21,4 @@ int main(void)
 	}
 	putchar('\n');
 		return (0);
+}
