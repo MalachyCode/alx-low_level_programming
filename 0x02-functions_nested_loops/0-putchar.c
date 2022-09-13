fe24e862227bbfd738055c90ddbbdf8126932b77
Malachy Nwafor <nwaformalachy2@gmail.com>
2022-09-13 14:20:27 -0700

    A program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
-#include "main.h"
+#include "main.h";
 /**
- * main - Entry point
- * Description: program to print _putchar
+ * main - prints putchar
  * _putchar: replaces prinf and is used to print
  * Return: Always 0 (success)
  */
 
-int _putchar(char c)
+int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
