ebfd6e8975275e823cef92b440444316b5fb5fea
Malachy Nwafor <nwaformalachy2@gmail.com>
2022-09-28 10:19:48 -0700

    A recursive function that returns the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,7 +3,7 @@
 /**
  * isSqrt - helper function for _sqrt_recursion
  * @n: input number
- * @i: iterator
+ * @i: iteratotr
  * Return: square root of number
  */
 
@@ -12,12 +12,12 @@ int isSqrt(int n, int i)
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
-		return (1);
+		return (i);
 	return (isSqrt(n, i + 1));
 }
 
 /**
- * _sqrt_recursion - returns the the natural square root of a number.
+ * _sqrt_recursion - returns the natural square root of a number
  * @n: input number
  * Return: square root of number, -1 if false
  */
