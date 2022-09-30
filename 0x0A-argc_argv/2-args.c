10b037709924b878fba12070890ebf0b42f2eb41
Malachy Nwafor <nwaformalachy2@gmail.com>
2022-09-30 08:34:26 -0700

    A program that prints all arguments it receives including the first one.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives including the first one.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Alway 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	return (0);
+}
