486cb0c33e988d52a9ef487beba7bd591cb1896f
Malachy Nwafor <nwaformalachy2@gmail.com>
2022-10-03 10:48:40 -0700

    A function (and include malloc) that creates an array of chars, and initializes it with a specific char.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,6 +18,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	ptr = malloc(sizeof(char) * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
